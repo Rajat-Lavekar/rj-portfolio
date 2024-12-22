1b9216377f7c61d7b6dbcdf1f29d6d549db063b6
Rajat-Lavekar <rajat.lavekar@gmail.com>
2024-12-22 14:38:40 +0530

    Initial commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import "./app.scss"
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section>Hero</section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Parallax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfoloio3</section>
+    <section>Contact</section>
+  </div>
+  // return <div>Hello World</div>;
 };
 
 export default App;
