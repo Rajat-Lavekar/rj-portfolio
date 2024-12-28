d1329ac1a7b969bb79e0f8f0e3e57c850b271224
Rajat-Lavekar <rajat.lavekar@gmail.com>
2024-12-28 23:05:18 +0530

    Links section constructed

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,6 +1,18 @@
 
 export const Links = () => {
+  const items = [
+    "Homepage",
+    "Services",
+    "Portfolio",
+    "Contact",
+    "About"
+  ];
   return (
-    <div>Links</div>
+    <div className="links">{items.map(item=>(
+      <a href={`#${item}`} key={item}>
+        {item}  
+      </a>
+    ))}
+    </div>
   )
 }
