598486fbb4b3267e69a81e78f639cc976e2c5dff
Rajat-Lavekar <rajat.lavekar@gmail.com>
2025-01-01 19:51:04 +0530

    Parallax section made mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -14,17 +14,17 @@ const App = () => {
       <Navbar />
       <Hero />
     </section>
-    {/* <section id="Services">
+    <section id="Services">
       <Parallax type="services"/>
     </section>
-    <section>
+    {/* <section>
       <Services />
-    </section>
-    <section id="Portfolio">
+    </section> */}
+    {/* <section id="Portfolio">
       <Parallax type="portfolio"/>
     </section>
-    <Portfolio />
-    <section id="Contact">
+    <Portfolio /> */}
+    {/* <section id="Contact">
       <Contact />
     </section> */}
     {/* <Test/> */}
