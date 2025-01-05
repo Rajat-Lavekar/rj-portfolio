378d51eb40d689d1989985369af944e91a6cb952
Rajat-Lavekar <rajat.lavekar@gmail.com>
2025-01-05 15:50:32 +0530

    Portfolio section made mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,13 @@ const App = () => {
     <section id="Services">
       <Parallax type="services"/>
     </section>
-    {/* <section>
+    <section>
       <Services />
-    </section> */}
-    {/* <section id="Portfolio">
+    </section>
+    <section id="Portfolio">
       <Parallax type="portfolio"/>
     </section>
-    <Portfolio /> */}
+    <Portfolio />
     {/* <section id="Contact">
       <Contact />
     </section> */}
